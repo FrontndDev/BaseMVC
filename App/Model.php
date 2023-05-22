8dd0373f3e5f8d89dca851da6ab60daa8a5e20a1
Egor-Babashko <egor.babashko2005@mail.ru>
2023-05-22 23:21:02 +0300

    commit

--- App/Model.php
+++ App/Model.php
@@ -12,4 +12,19 @@ abstract class Model
         $sql = 'SELECT * FROM ' . static::TABLE;
         return $db->query($sql, [], static::class);
     }
+
+    public static function findById($id) {
+        $db = new Db();
+        $sql = 'SELECT * FROM ' . static::TABLE .  " WHERE id IN($id)";
+        $result = $db->query($sql, [], static::class);
+
+        return empty($result) ? false : $result[0];
+    }
+
+    public static function getLastThreeRecords() {
+        $db = new Db();
+//        SELECT * FROM table_name ORDER BY id DESC LIMIT 0,100;
+        $sql = 'SELECT * FROM ' . static::TABLE . ' ORDER BY id DESC LIMIT 0, 3';
+        return $db->query($sql, [], static::class);
+    }
 }
--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require __DIR__ . '/autoload.php';
 
 $users = \App\Models\User::findAll();
 $articles = \App\Models\Article::findAll();
+$article = \App\Models\Article::findById(1);
+$last_three_articles = \App\Models\Article::getLastThreeRecords();
 
 ?>
 
@@ -11,19 +13,20 @@ $articles = \App\Models\Article::findAll();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
     <style>
         .wrapper {
-            display: flex;
-            column-gap: 50px;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 50px;
             padding: 25px;
         }
 
         .wrapper > div {
-            width: 50%;
+            margin: 0 auto;
             text-align: center;
         }
     </style>
@@ -33,17 +36,41 @@ $articles = \App\Models\Article::findAll();
     <div>
         <h1>Users</h1>
 
+        <?php foreach ($users as $user): ?>
         <div class="content">
-            <?=var_dump($users)?>
+            <h4><?=$user->id?>.  <?=$user->name?></h4>
+            <p>email: <?=$user->email?></p>
         </div>
+
+        <?php endforeach; ?>
     </div>
     <div>
         <h1>Articles</h1>
 
+        <?php foreach ($articles as $item): ?>
         <div class="content">
-            <?=var_dump($articles)?>
+            <a href="article.php?id=<?=$item->id?>"><?=$item->title?></a>
+        </div>
+        <?php endforeach; ?>
+    </div>
+
+    <div>
+        <h1>Article By Id</h1>
+
+        <div class="content">
+            <?=var_dump($article)?>
+        </div>
+    </div>
+
+    <div>
+        <h1>Last Three Articles</h1>
+
+        <div class="content">
+            <?=var_dump($last_three_articles)?>
         </div>
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
